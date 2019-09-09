88fd9baa36094c2a642a4fe8fa068ad54faa9e66
Hashed <ZD-Development@users.noreply.github.com>
2019-09-09 00:03:04 -0400

    Add paramaters

--- index.js
+++ index.js
@@ -1,22 +1,49 @@
 //jshint esversion: 6
 const express = require("express");
 const bodyParser = require("body-parser");
+const request = require("request");
 
 const app = express();
 
-app.use('/styles.css', express.static(__dirname + '/styles.css'));
+app.use("/styles.css", express.static(__dirname + "/styles.css"));
 
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
+app.use(
+    bodyParser.urlencoded({
+        extended: true
+    })
+);
 
 app.get("/", function(req, res) {
     res.sendFile(__dirname + "/index.html");
 });
 
 app.post("/", function(req, res) {
+    var crypto = req.body.crypto;
+    var fiat = req.body.fiat;
+    var amount = req.body.amount;
 
-    console.log(req.body.crypto);
+    var options = {
+        url: "https://apiv2.bitcoinaverage.com/convert/global",
+        method: "GET",
+        qs: {
+            from: crypto,
+            to: fiat,
+            amount: amount
+        }
+    };
+
+    request(options, function(error, response, body) {
+        var data = JSON.parse(body);
+        var price = data.price;
+
+        var currentDate = data.time;
+
+        res.write("<p>The current date is " + currentDate + "</p>");
+
+        res.write("<h1>" + amount + crypto + " is " + price + fiat + "</h1>");
+
+        res.send();
+    });
 });
 
 app.listen(3000, function() {
